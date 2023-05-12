b9b2b4ef1194b4243bce935a3e6121dc8b03fe02
AmmarNadeemCS <ammarnadeem2595@gmail.com>
2023-05-13 00:39:03 +0500

    init

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,26 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\layout.tsx": [
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\page": [
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\globals.css",
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\Aboutu\\page": [
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\globals.css",
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\Blog\\page": [
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\globals.css",
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\Aboutu\\services\\page": [
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\src\\app\\globals.css",
+      "C:\\Users\\Ammaar\\Desktop\\nodeproject\\webmap\\helloworld\\webmap\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
